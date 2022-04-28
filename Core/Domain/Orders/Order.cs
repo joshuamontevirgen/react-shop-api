b3049f77627cf0ed36af315605f91bf0173aeae3
joshuamontevirgen <joshua2936@gmail.com>
2022-04-28 23:07:45 +0800

    remove id

--- Core/Domain/Orders/Order.cs
+++ Core/Domain/Orders/Order.cs
@@ -4,7 +4,6 @@ namespace Core.Domain.Orders
 {
     public class Order : BaseEntity
     {
-        public int Id { get; set; }
         public int UserId { get; set; }
         public DateTime Date { get; set; }
         public OrderStatus OrderStatus { get; set; }
--- Core/Domain/Orders/OrderItem.cs
+++ Core/Domain/Orders/OrderItem.cs
@@ -2,7 +2,6 @@
 {
     public class OrderItem : BaseEntity
     {
-        public int Id { get; set; }
         public int OrderId { get; set; }
         public int ItemId { get; set; }
         public int Quantity { get; set; }
